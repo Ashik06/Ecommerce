4582c46e499a274497d46bc6549e02193fec62ee
Ashik06 <35214432+Ashik06@users.noreply.github.com>
2018-01-08 12:46:40 +0530

    8/1/2018

---
+++ Backend/src/main/java/com/niit/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Product;
+
+public interface ProductDao {
+List<Product> getAllProducts(); //select * from product
+ Product getProduct( int id);//select* from product where id=1
+ 
+void deleteProduct(int id);
+}
---
+++ Backend/src/main/java/com/niit/model/Category.java
@@ -0,0 +1,35 @@
+package com.niit.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String categoryname;
+	
+	@OneToMany
+	private List<Product> products;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCategoryname() {
+		return categoryname;
+	}
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+
+}
